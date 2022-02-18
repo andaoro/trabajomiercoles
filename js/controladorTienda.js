36c60abee84966bbda5391c8ea1f31ce7e08b19c
andaoro <andaoro1@gmail.com>
2022-02-18 16:41:06 -0500

    avance

---
+++ js/controladorTienda.js
@@ -0,0 +1,36 @@
+//entradas problema
+//entrada == vriables == constantes
+
+let nombreproducto="peluche"
+let precioProducto=400000
+let pesoProducto= 18.5
+let tieneDescuento=true
+let descripcionProducto="Peluche de tanjiro"
+let telefonoVendedor="3012574242"
+
+const NOMBRE_CLIENTE="Juan Jose"
+//arreglos unidimensionales (vectores)
+//sirven para almacenar varios elementos en una variable
+
+let productos=["peluche","camiseta","figuras","mochila"]
+
+//orientada a objetos
+
+let productos2=Array("peluche","camiseta","figuras","mochila")
+
+
+
+//salidas
+console.log("Bienvenido "+NOMBRE_CLIENTE)
+console.log("Esta seguro que desea comprar "+nombreproducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es: ${nombreproducto}`)
+
+console.log("**************************")
+
+console.log(productos)
+console.log(productos2[1])
+
+
+
+
+
