741fbe83364f64978dd919a6d89e2478d45cf863
andaoro <andaoro1@gmail.com>
2022-03-01 20:07:09 -0500

    Añadir articulos de la tienda en JS

--- js/llenadotienda.js
+++ js/llenadotienda.js
@@ -2,8 +2,16 @@
 //crear 10 productos
 let productos=
 [
-     {nombre:"peluche",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje..."},
-     {nombre:"camiseta",precio:80000,descripcion:"Camisa estampada de alta calidad"}
+     {nombre:"peluche Tanjiro",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Kamado tanjiro"},
+     {nombre:"peluche Rengoku",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Kyoujuro Rengoku"},
+     {nombre:"peluche Douma ",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Douma"},
+     {nombre:"camiseta",precio:80000,descripcion:"Camisa estampada de alta calidad"},
+     {nombre:"Figuras",precio:110000,descripcion:"Figuras chibi del anime kimetsu No Yaiba"},
+     {nombre:"Aretes",precio:12000,descripcion:"Aretes de los personajes del anime kimetsu No Yaiba"},
+     {nombre:"Kimonos",precio:65000,descripcion:"kimonos de distintas tallas de los personajes del anime kimetsu No Yaiba"},
+     {nombre:"Lamparas",precio:30000,descripcion:"Lamparas led de los personajes del anime kimetsu No Yaiba"},
+     {nombre:"Mangas",precio:25000,descripcion:"Tomos del manga (1 - 23) del anime Kimetsu No Yaiba"},
+     {nombre:"Llaveros",precio:15000,descripcion:"Llaveros metalicos de la tematica del anime"},
 
 ]
 
