9dade0809461245d6057d89a9fd5865600ec475a
andaoro <andaoro1@gmail.com>
2022-03-14 11:51:05 -0500

    Primera presentacion

--- js/llenadotienda.js
+++ js/llenadotienda.js
@@ -2,16 +2,16 @@
 //crear 10 productos
 let productos=
 [
-     {foto: 'img/articulos/peluche_tanjiro.jpg', nombre:"peluche Tanjiro",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Kamado tanjiro"},
-     {foto: 'img/articulos/peluche_rengoku.jpg', nombre:"peluche Rengoku",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Kyoujuro Rengoku"},
-     {foto: 'img/articulos/peluche_douma.jpg', nombre:"peluche Douma ",precio:120000,descripcion:"Peluche de 20 cms basado en el personaje Douma"},
-     {foto: 'img/articulos/camiseta.jpg', nombre:"camiseta",precio:80000,descripcion:"Camisa estampada de alta calidad"},
-     {foto: 'img/articulos/Figuras.jpg', nombre:"Figuras",precio:110000,descripcion:"Figuras chibi del anime kimetsu No Yaiba"},
-     {foto: 'img/articulos/aretes.jpg', nombre:"Aretes",precio:12000,descripcion:"Aretes de los personajes del anime kimetsu No Yaiba"},
-     {foto: 'img/articulos/kimono.jpg', nombre:"Kimonos",precio:65000,descripcion:"kimonos de distintas tallas de los personajes del anime kimetsu No Yaiba"},
-     {foto: 'img/articulos/lamparas.jpg', nombre:"Lamparas",precio:30000,descripcion:"Lamparas led de los personajes del anime kimetsu No Yaiba"},
-     {foto: 'img/articulos/mangas.jpg', nombre:"Mangas",precio:25000,descripcion:"Tomos del manga (1 - 23) del anime Kimetsu No Yaiba"},
-     {foto: 'img/articulos/llaveros.jpg', nombre:"Llaveros",precio:15000,descripcion:"Llaveros metalicos de la tematica del anime"},
+     {foto: 'img/articulos/tanjirof.jpg', nombre:"Figura Tanjiro",precio:"$"+80000,descripcion:"Figura de 20cms del personaje Kamado tanjiro."},
+     {foto: 'img/articulos/zenitsuf.jpg', nombre:"Figura Zenitsu",precio:"$"+80000,descripcion:"Figura de 20 cms basado en el personaje Zenitsu agatsuma."},
+     {foto: 'img/articulos/nezukof.jpg', nombre:"Figura Nezuko ",precio:"$"+80000,descripcion:"Figura de 20cms basada en el personaje Kamado Nezuko."},
+     {foto: 'img/articulos/cartuchera.jpg', nombre:"Cartuchera",precio:"$"+30000,descripcion:"Cartuchera Grande kimetsu No yaiba."},
+     {foto: 'img/articulos/billetera.jpg', nombre:"Billeteras",precio:"$"+25000,descripcion:"Billetera Zenitsu Kimetsu No yaiba."},
+     {foto: 'img/articulos/accesorios.jpg', nombre:"Accesorios",precio:"$"+20000,descripcion:"Caja de Accesorios Variados Kimetsu No yaiba."},
+     {foto: 'img/articulos/zenitsufunko.jpg', nombre:"Funko Pop Zenitsu",precio:"$"+70000,descripcion:"Funko pop zenitsu agamatsu Original."},
+     {foto: 'img/articulos/inosukefunko.jpg', nombre:"Funko Pop Inosuke ",precio:"$"+70000,descripcion:"Funko pop Inosuke Hashibara Original."},
+     {foto: 'img/articulos/medias.jpg', nombre:"Medias",precio:"$"+7000,descripcion:"Medias Inosuke Hashibara Kimetsu No yaiba."},
+     {foto: 'img/articulos/uzui.jpg', nombre:"Figura Tengen Uzui",precio:"$"+75000,descripcion:"Figura de Tengen Uzui pilar del sonido."},
 
 ]
 //1. creo una variable para almacenar la base para pintar
@@ -30,9 +30,11 @@ productos.forEach(function(producto){
      let tarjeta=document.createElement("div")
      tarjeta.classList.add("card")
      tarjeta.classList.add("h-100")
+     tarjeta.classList.add("bg-dark")
 
      let foto=document.createElement("img")
      foto.classList.add("card-img-top")
+     foto.classList.add("img-fluid")
      foto.src=producto.foto
      
      let nombre=document.createElement("h4")
@@ -40,13 +42,28 @@ productos.forEach(function(producto){
      nombre.textContent=producto.nombre
 
      let descripcion = document.createElement("p")
+     descripcion.classList.add("text-left")
      descripcion.textContent=producto.descripcion
+     
+     let precio = document.createElement("h4")
+     precio.classList.add("text-center")
+     precio.classList.add("text-warning")
+     precio.classList.add("number")
+     precio.textContent=producto.precio
+
+     let comprar = document.createElement("button")
+     comprar.classList.add("bg-success")
+     comprar.classList.add("text-light")
+     comprar.textContent="Comprar"
+
 
      //PADRES E HIJOS
 
      tarjeta.appendChild(nombre)
      tarjeta.appendChild(foto)
+     tarjeta.appendChild(precio)
      tarjeta.appendChild(descripcion)
+     tarjeta.appendChild(comprar)
      columna.appendChild(tarjeta)
      filas.appendChild(columna)
 })
